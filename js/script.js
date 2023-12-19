7ae343072e42a750ed6f7bea81d89b0c8dfbdf4c
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2023-12-19 17:58:43 +0100

    add JS - HTML user imput

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
 console.log('JS OK')
+
+/* 
+//Recupero dell'elemento di "stampa" nel DOM
+document.getElementById('userMessage');
+console.log(userMessage);
+
+//Variabili di imput da parte dell'utente
+let userKm = parseInt(prompt("Inserisci il numero di chilometri:", 800));
+let userAge = parseInt(prompt("Inserisci la tua età a numero:", 26));
+console.log("kilometri utente: " + userKm,"Età utente: " + userAge);
+
+//Controllo per l'inserimento numero valido KM
+if (isNaN(userKm) || userKm <= 0) {
+    userKm = parseInt(prompt("Inserisci un numero di chilometri valido (deve essere un NUMERO e MAGGIORE di ZERO(0)):"));
+}
+
+// Controllo per l'inserimento numero valido per l'età
+if (isNaN(userAge) || userAge <= 0) {
+    userAge = parseInt(prompt("Inserisci un'età valida (deve essere un NUMERO e MAGGIORE di ZERO(0)):"));
+}
+
+//Variabile contenete il prezzo standard per km
+const ticketStandardPrice = 0.21;
+
+//calcolo del prezzo standard per km senza applicare sconti
+let totPrice = userKm * ticketStandardPrice;
+
+
+if(userAge < 18){
+    const minors = 0.2;
+    totPrice = totPrice - (totPrice * minors);
+} else if(userAge >= 65){
+    const over = 0.4;
+    totPrice = totPrice - (totPrice * over);
+}
+//Stampa del prezzo finale in cosole e in pagina
+console.log("Prezzo finale: " + totPrice.toFixed(2));
+userMessage.innerHTML = `<strong>Il prezzo finale della tua tratta è:</strong> ${totPrice.toFixed(2)} euro`
+*/
+
+const firstNameInput = document.getElementById('userFirstName');
+const lastNameInput = document.getElementById('userLastName');
+const kmsInput = document.getElementById('userKms');
+const ageInput = document.getElementById('userAge');
+const calcBtn = document.getElementById('calcBtn');
+
+const ticketStandardPrice = 0.21;
+
+calcBtn.addEventListener('click', function(){
+    let firstName = firstNameInput.value;
+    let lastName = lastNameInput.value;
+    let kms = kmsInput.value;
+    let age = ageInput.value;
+
+    console.log(firstName, lastName, kms, age,);
+});
